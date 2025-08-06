65b77d2ab0ec5cfb45945e364a927c1d94708673
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2025-08-06 10:14:40 -0500

    Create build-script-colab.sh

---
+++ build-script-colab.sh
@@ -0,0 +1,12 @@
+git submodule update --init --recursive --remote 
+apt install cmake libprotobuf-c-dev python3-protobuf libprotobuf23 libprotobuf-dev libgoogle-glog-dev 
+mkdir build && cd build 
+cmake \
+  -DOpenCV_CONFIG_FILE=/opt/opencv/build/OpenCVConfig.cmake \
+  -DDOWNLOAD_BODY_25_MODEL=OFF \
+  -DDOWNLOAD_FACE_MODEL=OFF \
+  -DDOWNLOAD_HAND_MODEL=OFF \
+  -DBUILD_EXAMPLES=OFF \
+  -DBUILD_PYTHON=ON \
+  ../ 
+make -j$nrpoc
