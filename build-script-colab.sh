a753f4596ba5480ff9171077b6595201e1b808bb
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2025-08-06 11:19:08 -0500

    Update build-script-colab.sh

--- build-script-colab.sh
+++ build-script-colab.sh
@@ -1,7 +1,7 @@
 git submodule update --init --recursive --remote 
 apt install cmake libprotobuf-c-dev python3-protobuf libprotobuf23 libprotobuf-dev libgoogle-glog-dev libopencv-dev
 mkdir -p /opt/opencv/build
-cp /usr/lib/x86_64-linux-gnu/cmake/opencv4/* /opt/opencv/build/*
+cp /usr/lib/x86_64-linux-gnu/cmake/opencv4/* /opt/opencv/build/
 mkdir build && cd build 
 cmake \
   -DOpenCV_CONFIG_FILE=/opt/opencv/build/OpenCVConfig.cmake \
